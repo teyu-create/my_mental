c2f91449d955693cee408dfce953fd65fc5f0533
yuria <aw0asgt@gmail.com>
2024-11-28 08:44:05 +0000

    bladeの継承

--- resources/views/guest/mental/create.blade.php
+++ resources/views/guest/mental/create.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/guest.blade.phpを読み込む --}}
+@extends('layouts.guest')
 
-        <title>メンタル記録</title>
-    </head>
-    <body>
-        <h1>今日の気分は？</h1>
-    </body>
-</html>
+
+{{-- guest.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'メンタル記録')
+
+{{-- guest.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>今日の気分は？</h2>
+            </div>
+        </div>
+    </div>
+@endsection
