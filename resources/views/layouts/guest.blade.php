6c9bcc4e692ccc968d033939f6c180aaad522a62
yuria <aw0asgt@gmail.com>
2024-11-29 09:52:16 +0000

    ロゴをセンター配置

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -30,24 +30,10 @@
         <div id="app">
             {{-- 画面上部に表示するナビゲーションバーです。 --}}
             <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
-                <div class="w-100 p-3">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                <div class="container justify-content-center">
+                    <a class="navbar-brand m-0" href="{{ url('/') }}">
+                     <img src= "{{ asset('image/まいにちのメンタル.png') }}"  width="800" alt="まいにちのメンタルロゴ" />
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav ms-auto">
-
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav">
-                        </ul>
-                    </div>
                 </div>
             </nav>
             {{-- ここまでナビゲーションバー --}}
