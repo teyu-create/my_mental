394fc742c620f4283e79b4191a6ee8aa172b00ae
yuria <aw0asgt@gmail.com>
2024-12-04 14:56:39 +0000

    下線追加

--- resources/views/guest/mental/create.blade.php
+++ resources/views/guest/mental/create.blade.php
@@ -11,28 +11,32 @@
       <div class="col-md-8 mx-auto">
         <h2>今日の気分は？</h2>
       </div>
-      <div class="d-flex justify-content-between">
-        <img src="{{ asset('image/晴れマーク.png') }}" style="
-            height: 136px;
-            padding-top: 10px;">
-        <img src="{{ asset('image/くもりマーク.png') }}" style="
-            height: 141px;
-            padding-top: 50px;">
-        <img src="{{ asset('image/雨マーク.png') }}">
-     </div>
-      <div class="d-flex justify-content-between">
-            <div class="form-check form-check-inline">
-             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 
-             <label class="form-check-label" for="inlineRadio1">晴れ</label>
-            </div>
-            <div class="form-check form-check-inline">
-             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-             <label class="form-check-label" for="inlineRadio2">くもり</label>
-            </div>
-            <div class="form-check form-check-inline">
-             <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
-             <label class="form-check-label" for="inlineRadio3">雨</label>
-            </div>
+      <div class="dotline">
+        <div class ="mental_choice">
+          <div class="d-flex justify-content-between">
+            <img src="{{ asset('image/晴れマーク.png') }}" style="
+                height: 136px;
+                padding-top: 10px;">
+            <img src="{{ asset('image/くもりマーク.png') }}" style="
+                height: 141px;
+                padding-top: 50px;">
+            <img src="{{ asset('image/雨マーク.png') }}">
+          </div>
+          <div class="d-flex justify-content-between"style="padding-bottom: 30px;">
+                <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 
+                <label class="form-check-label" for="inlineRadio1">晴れ</label>
+                </div>
+                <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                <label class="form-check-label" for="inlineRadio2">くもり</label>
+                </div>
+                <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+                <label class="form-check-label" for="inlineRadio3">雨</label>
+                </div>
+          </div>
+        </div>
       </div>
   </div>
 @endsection
