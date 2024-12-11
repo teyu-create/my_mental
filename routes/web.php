a7f43b518132e9500d64aaaf82270fc74371112d
yuria <aw0asgt@gmail.com>
2024-12-11 10:52:27 +0000

    トップ画面表示（ただしログイン後でも表示される状態）

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.top');
 });
 
 use App\Http\Controllers\Guest\MentalController;
