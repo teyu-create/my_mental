35d3e4d02238c8cc0e3018d5ff0f446f0c54d185
yuria <aw0asgt@gmail.com>
2025-01-10 03:08:57 +0000

    一覧画面のレイアウト調整

--- resources/views/guest/mental/list.blade.php
+++ resources/views/guest/mental/list.blade.php
@@ -30,42 +30,42 @@
         </div>
         <div class="row">
                     <table class="table table-dark">
-                        <thead>
+                        <thead align="center">
                             <tr>
-                                <th width="28%"></th>
+                                <th></th>
                                 <th>気分</th>
                                 <!--<th width="10%">睡眠時間</th>
                                 <th width="10%">起床時間</th>-->
                                 <th>ごはん</th>
                                 <th>お仕事／学校</th>
-                                <th width="27%"></th>
+                                <!--<th width="27%"></th>-->
                                 <!--<th width="10%">どんな1日？</th>-->
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($posts as $mental)
                                 <tr>
-                                    <td>{{ Str::limit($mental->created_at,10,"") }}</td>
+                                    <td class="mental-table-body">{{ Str::limit($mental->created_at,10,"") }}</td>
                                     @if (false !== strpos("晴れ",$mental->mental_weather))
-                                      <td><img src="{{ asset('image/晴れちゃん.png') }}" style="width: 42px;"></td>
+                                      <td class="mental-table-body"><img src="{{ asset('image/晴れちゃん.png') }}" style="width: 42px;"></td>
                                      @elseif(false !== strpos("くもり",$mental->mental_weather))
-                                      <td><img src="{{ asset('image/くもりちゃん.png') }}" style="width: 42px;"></td>
+                                      <td class="mental-table-body"><img src="{{ asset('image/くもりちゃん.png') }}" style="width: 42px;"></td>
                                      @else
-                                      <td><img src="{{ asset('image/雨ちゃん.png') }}" style="width: 42px;"></td>
+                                      <td class="mental-table-body"><img src="{{ asset('image/雨ちゃん.png') }}" style="width: 42px;"></td>
                                     @endif
                                     <!--<td>{{ $mental->sleep_time }}</td>
                                     <td>{{ $mental->up_time }}</td>-->
                                     @if (!is_array($mental->eat)){{--もしeatが配列じゃなかったら、中身をそのまま表示--}}
-                                    <td>{{ $mental->eat }}</td>
+                                    <td class="mental-table-body">{{ $mental->eat }}</td>
                                     @else
-                                    <td>{{ implode(",", $mental->eat) }}</td>{{--配列の場合、中身を","で区切って文字列として表示--}}
+                                    <td class="mental-table-body">{{ implode(",", $mental->eat) }}</td>{{--配列の場合、中身を","で区切って文字列として表示--}}
                                     @endif
-                                    <td>{{ $mental->go_or_home }}</td>
-                                    <td>
-                                     <a href="{{ route('mental.edit', ['id' => $mental->id]) }}" role="button" class="btn btn-primary btn-sm">編集/詳細</a>
-                                    </td>
-                                    <!--<td>{{ Str::limit($mental->diary, 250,"…") }}</td>-->
+                                    <td class="mental-table-body">{{ $mental->go_or_home }}</td>
                                 </tr>
+                                     <td colspan="5" align="right">
+                                         <a href="{{ route('mental.edit', ['id' => $mental->id]) }}" role="button" class="btn btn-primary btn-sm">編集/詳細</a>
+                                     </td>
+                                    <!--<td>{{ Str::limit($mental->diary, 250,"…") }}</td>-->
                             @endforeach
                         </tbody>
                     </table>
